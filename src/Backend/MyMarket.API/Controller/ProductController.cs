b0f4ad260ddf44d6450241e27bf0976ed0718c92
kelvinkrause <kelvinrkrause@outlook.com>
2026-01-09 18:50:18 +0000

    feat: create product controller

---
+++ src/Backend/MyMarket.API/Controller/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMarket.Communication.Requests;
+using MyMarket.Communication.Response;
+
+namespace MyMarket.API.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisteredProductJson request)
+        {
+            var response = new ResponseRegisteredProductJson
+            {
+                Name = string.Empty
+            };
+
+            return Created(string.Empty, response);
+        }
+    }
+}
---
+++ src/Shared/MyMarket.Communication/Requests/RequestRegisteredProductJson.cs
@@ -0,0 +1,15 @@
+﻿namespace MyMarket.Communication.Requests
+{
+    public class RequestRegisteredProductJson
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+        public int StockQuantity { get; set; }
+
+        public string Category { get; set; }
+        public string Barcode { get; set; }
+
+    }
+}
