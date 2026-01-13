6100461a87a352cdd083d93ce00addbc3e9fd789
kelvinkrause <kelvinrkrause@outlook.com>
2026-01-13 21:41:40 +0000

    Started the product service use case

--- src/Backend/MyMarket.API/Controller/ProductController.cs
+++ src/Backend/MyMarket.API/Controller/ProductController.cs
@@ -1,20 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyMarket.Application.UseCase.Product.Register;
 using MyMarket.Communication.Requests;
 using MyMarket.Communication.Response;
 
 namespace MyMarket.API.Controller
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class ProductController : ControllerBase
     {
         [HttpPost]
-        public IActionResult Register([FromBody] RequestRegisteredProductJson request)
+        public async Task<IActionResult> RegisterAsync([FromBody] RequestRegisteredProductJson request)
         {
-            var response = new ResponseRegisteredProductJson
-            {
-                Name = string.Empty
-            };
+
+            var useCase = new RegisterProductUseCase();
+
+            var response = await useCase.Execute(request);
 
             return Created(string.Empty, response);
         }
