5b2564907553379025930e8c9718fe85ba239e63
kelvinkrause <kelvinrkrause@outlook.com>
2026-02-07 23:10:45 +0000

    feat: Created Migrations Configuration

--- src/Backend/MyMarket.Infrastructure/Migrations/DatabaseMigration.cs
+++ src/Backend/MyMarket.Infrastructure/Migrations/DatabaseMigration.cs
@@ -1,16 +1,33 @@
 ﻿using Dapper;
+using FluentMigrator;
+using FluentMigrator.Runner;
 using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore.Migrations.Operations;
-using System.Data.Common;
-using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MyMarket.Infrastructure.Migrations
 {
     public static class DatabaseMigration
     {
-        public static void Migrations(string connetionString)
+        public static void Migrations(
+            string connetionString,
+            IServiceProvider serviceProvider)
         {
             EnsureDatabaseCreated(connetionString);
+            MigrateDatabase(serviceProvider);
+        }
+
+        // This method is called after the application has started and the dependency injection
+        // container has been built, so we can resolve the IMigrationRunner service from the container to run
+        // the migrations.
+        private static void MigrateDatabase(IServiceProvider serviceProvider)
+        {
+            // Resolve the IMigrationRunner service from the dependency injection container to run the migrations.
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+            // List all the migrations that are available in the assembly and have not been applied to the database yet.
+            runner.ListMigrations();
+            // Run the migrations to update the database schema to the latest version.
+            // This will apply any pending migrations that have not been applied yet.
+            runner.MigrateUp();
         }
 
         private static void EnsureDatabaseCreated(string connetionString)
